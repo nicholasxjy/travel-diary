ca249012f5921263175b5e5088e4e62b1a568c3b
nicholasxjy <yitaqi2011@gmail.com>
2014-10-02 00:59:47 +0800

    add some

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,18 +12,19 @@ angular
   .module('travelDiaryApp', [
     'ui.router',
     'ngResource',
-    'avoscloud'
+    'avoscloud',
+    'app.controllers'
   ])
   .config(['$stateProvider', '$urlRouterProvider', 'avoscloudProvider', function($stateProvider, $urlRouterProvider, avoscloudProvider) {
      avoscloudProvider.config({
       'X-AVOSCloud-Application-Id': 'n5kqlr9jl2xu4lw5ewn4zib9kx02iqxbo20gqr36soo8qrem',
       'X-AVOSCloud-Application-Key': '5upx5pwbr2u49wsgztcjauvwkzot1e9oe18mmioqm1hy3pkl'
     });
-     $urlRouterProvider.otherwise("/");
+     $urlRouterProvider.otherwise('/');
      $stateProvider
-        .state("welcome", {
-          url: "/",
-          templateUrl: "views/welcome.html",
-          controller: "WelcomeController"
-        })
-  }])
+        .state('welcome', {
+          url: '/',
+          templateUrl: 'views/welcome.html',
+          controller: 'WelcomeController'
+        });
+  }]);
--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -1,4 +1,28 @@
-angular.module('travelDiaryApp')
-	.controller('WelcomeController', ['avoscloud', '$scope', function(avoscloud, $scope){
-		
-	}])
+var app = angular.module('app.controllers', ['ngDialog']);
+app.controller('WelcomeController', function($scope, ngDialog){
+    'use strict';
+	$scope.showLoginModal = function() {
+        ngDialog.open({
+            template: 'views/partials/loginform.html',
+            controller: 'LoginController'
+        });
+    };
+});
+
+app.controller('LoginController', function($scope, ngDialog) {
+   'use strict';
+    $scope.showForgetPassModal = function() {
+        ngDialog.close('ngdialog1');
+        ngDialog.open({
+            template: 'views/partials/forgetpass.html',
+            controller: 'ForgetController'
+        });
+    };
+});
+
+app.controller('ForgetController', function($scope, ngDialog) {
+   'use strict';
+    $scope.closeForgetPassModal = function() {
+        ngDialog.close('ngdialog1');
+    };
+});
