b528b539165df926a54b04a12134a3a96b319098
nicholasxjy <yitaqi2011@gmail.com>
2014-11-07 20:27:19 +0800

    user page

--- app/scripts/controllers/user_controller.js
+++ app/scripts/controllers/user_controller.js
@@ -9,17 +9,49 @@
             'UserService',
             'FeedService',
             '$stateParams',
+            '$sce',
             userCtrl
         ]);
 
-        function userCtrl($scope, ngDialog, $state, UserService, FeedService, $stateParams) {
+        function userCtrl($scope, ngDialog, $state, UserService, FeedService, $stateParams, $sce) {
             var userid = $stateParams.id;
             console.log(userid);
             UserService.findUserById(userid)
                 .then(function(user) {
                     FeedService.findFeedByUser(user)
                         .then(function(feeds) {
-                            console.log(feeds);
+                            var feedsArr = [];
+                            angular.forEach(feeds, function(feed) {
+                              var feedInfo = {};
+                              feedInfo.author = feed.get('author').toJSON();
+                              feedInfo.feed = feed.toJSON();
+                              if (feedInfo.feed.video) {
+                                feedInfo.feed.video.config = {
+                                  theme: {
+                                    url: 'bower_components/videogular-themes-default/videogular.css'
+                                  },
+                                  sources: [
+                                    {src: $sce.trustAsResourceUrl(feedInfo.feed.video.url), type: 'video/mp4'},
+                                    {src: $sce.trustAsResourceUrl(feedInfo.feed.video.url), type: 'video/webm'},
+                                    {src: $sce.trustAsResourceUrl(feedInfo.feed.video.url), type: 'video/ogg'}
+                                  ]
+                                }
+                              }
+                              if (feedInfo.feed.audio) {
+                                feedInfo.feed.audio.config = {
+                                  theme: {
+                                    url: 'bower_components/videogular-themes-default/videogular.css'
+                                  },
+                                  sources: [
+                                    {src: $sce.trustAsResourceUrl(feedInfo.feed.audio.url), type: "audio/mpeg"},
+                                    {src: $sce.trustAsResourceUrl(feedInfo.feed.audio.url), type: "audio/ogg"},
+                                  ]
+                                }
+                              }
+                              feedsArr.push(feedInfo);
+                            });
+                            $scope.feeds = feedsArr;
+                            console.log($scope.feeds);
                         })
                 })
         }
